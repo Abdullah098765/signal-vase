c51487c915a00b0fa8dfa9dd5dda9a11afc16641
Abdullah <ssmmhazz@gmail.com>
2023-09-22 13:26:29 +0500

    d'

--- src/app/api/db.js
+++ src/app/api/db.js
@@ -2,10 +2,12 @@
 
 import mongoose from 'mongoose';
 
+
+// mongodb+srv://sabir:LNnrcPrsOnfuTMEa@atlascluster.rimv5ng.mongodb.net/?retryWrites=true&w=majority
 // Connect to MongoDB
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://sabir:LNnrcPrsOnfuTMEa@atlascluster.rimv5ng.mongodb.net/?retryWrites=true&w=majority', {
+    await mongoose.connect('mongodb+srv://sabir:LNnrcPrsOnfuTMEa@atlascluster.rimv5ng.mongodb.net/test?retryWrites=true&w=majority&ssl=true', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
