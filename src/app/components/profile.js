664300051c8c41bc2cd8bb5509001ac66f9eb7a0
Abdullah <ssmmhazz@gmail.com>
2023-10-15 13:42:38 +0500

    JHAB

--- src/app/components/profile.js
+++ src/app/components/profile.js
@@ -1,5 +1,8 @@
 'use client'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useMyContext } from '../context/context';
+import { faChartArea } from '@fortawesome/free-solid-svg-icons';
+import { faChartBar } from '@fortawesome/free-regular-svg-icons';
 
 function User() {
     const { user } = useMyContext();
@@ -8,10 +11,10 @@ function User() {
         <div>
 
             <div class="h-full bg-gray-200 p-8">
-                <div class="bg-white rounded-lg shadow-xl p-6 flex lg:flex-row sm:flex-col md:flex-col items-center">
+                <div class="bg-white rounded-lg shadow-xl p-6 flex flex-col lg:flex-row  xl:flex-row items-center">
                     <div x-data="{ openSettings: false }" class="absolute right-12 mt-4 rounded">
                     </div>
-                    <img src="https://vojislavd.com/ta-template-demo/assets/img/profile.jpg" class="w-40 border-4 border-white rounded-full mx-4" />
+                    <img src={user.profilePicture} class="w-40 border-4 border-white rounded-full mx-4" />
                     <div>
                         <div class="flex items-center space-x-2 mt-2">
                             <p class="text-2xl">Amanda Ross</p>
@@ -25,7 +28,7 @@ function User() {
                         <p class="text-sm text-gray-500">New York, USA</p>
                     </div>
                     <div class="flex-1 flex flex-col items-center lg:items-end justify-end px-8">
-                        <div class="flex items-center space-x-4 mt-24 md:mt-3 sm:mt-3">
+                        <div class="flex items-center space-x-4 lg:mt-24 xl:mt-24 mt-3">
                             <button class="flex items-center bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                                     <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"></path>
@@ -43,93 +46,21 @@ function User() {
                 </div>
 
 
-                {/* <div class="bg-white rounded-lg shadow-xl pb-8">
-                    <div x-data="{ openSettings: false }" class="absolute right-12 mt-4 rounded">
-                        <button class="border border-gray-400 p-2 rounded text-gray-300 hover:text-gray-300 bg-gray-100 bg-opacity-10 hover:bg-opacity-20" title="Settings">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path>
-                            </svg>
-                        </button>
-                        <div x-show="openSettings" class="bg-white absolute right-0 w-40 py-2 mt-1 border border-gray-200 shadow-2xl hidden">
-                            <div class="py-2 border-b">
-                                <p class="text-gray-400 text-xs px-6 uppercase mb-1">Settings</p>
-                                <button class="w-full flex items-center px-6 py-1.5 space-x-2 hover:bg-gray-200">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"></path>
-                                    </svg>
-                                    <span class="text-sm text-gray-700">Share Profile</span>
-                                </button>
-                                <button class="w-full flex items-center py-1.5 px-6 space-x-2 hover:bg-gray-200">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"></path>
-                                    </svg>
-                                    <span class="text-sm text-gray-700">Block User</span>
-                                </button>
-                                <button class="w-full flex items-center py-1.5 px-6 space-x-2 hover:bg-gray-200">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                    </svg>
-                                    <span class="text-sm text-gray-700">More Info</span>
-                                </button>
-                            </div>
-                            <div class="py-2">
-                                <p class="text-gray-400 text-xs px-6 uppercase mb-1">Feedback</p>
-                                <button class="w-full flex items-center py-1.5 px-6 space-x-2 hover:bg-gray-200">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path>
-                                    </svg>
-                                    <span class="text-sm text-gray-700">Report</span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="w-full h-[250px]">
-                        <img src="https://vojislavd.com/ta-template-demo/assets/img/profile-background.jpg" class="w-full h-full rounded-tl-lg rounded-tr-lg" />
-                    </div>
-                    <div class="flex flex-col items-center -mt-20">
-                        <img src="https://vojislavd.com/ta-template-demo/assets/img/profile.jpg" class="w-40 border-4 border-white rounded-full" />
-                        <div class="flex items-center space-x-2 mt-2">
-                            <p class="text-2xl">Amanda Ross</p>
-                            <span class="bg-blue-500 rounded-full p-1" title="Verified">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="text-gray-100 h-2.5 w-2.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M5 13l4 4L19 7"></path>
-                                </svg>
-                            </span>
-                        </div>
-                        <p class="text-gray-700">Senior Software Engineer at Tailwind CSS</p>
-                        <p class="text-sm text-gray-500">New York, USA</p>
-                    </div>
-                    <div class="flex-1 flex flex-col items-center lg:items-end justify-end px-8 mt-2">
-                        <div class="flex items-center space-x-4 mt-2">
-                            <button class="flex items-center bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"></path>
-                                </svg>
-                                <span>Connect</span>
-                            </button>
-                            <button class="flex items-center bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fill-rule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clip-rule="evenodd"></path>
-                                </svg>
-                                <span>Message</span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                 */}
                 <div class="flex-1 w-full bg-white rounded-lg shadow-xl mt-4 p-8">
-                    <h4 class="text-xl text-gray-900 font-bold">Statistics</h4>
+                    <h4 class="text-xl text-gray-900 font-bold">Careere</h4>
 
                     <div class="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-4">
                         <div class="px-6 py-6 bg-gray-100 border border-gray-300 rounded-lg shadow-xl">
                             <div class="flex items-center justify-between">
-                                <span class="font-bold text-sm text-indigo-600">Total Revenue</span>
+                                <span class="font-bold text-sm text-indigo-600">Total Signals</span>
                                 <span class="text-xs bg-gray-200 hover:bg-gray-500 text-gray-500 hover:text-gray-200 px-2 py-1 rounded-lg transition duration-200 cursor-default">7 days</span>
                             </div>
                             <div class="flex items-center justify-between mt-6">
                                 <div>
-                                    <svg class="w-12 h-12 p-2.5 bg-indigo-400 bg-opacity-20 rounded-full text-indigo-600 border border-indigo-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                <svg class="w-12 h-12 p-2.5 bg-indigo-400 bg-opacity-20 rounded-full text-indigo-600 border border-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+<FontAwesomeIcon icon={faChartBar}/>
+    <path d="M12 4a2 2 0 100 4 2 2 0 000-4zM19.59 12l-6.3-6.3a2 2 0 00-2.83 0l-6.3 6.3a2 2 0 000 2.83l6.3 6.3a2 2 0 002.83 0l6.3-6.3a2 2 0 000-2.83z" />
+</svg>
                                 </div>
                                 <div class="flex flex-col">
                                     <div class="flex items-end">
