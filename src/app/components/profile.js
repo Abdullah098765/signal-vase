9786193e07d406afbe65e03e77a2527c0e00499a
Abdullah <ssmmhazz@gmail.com>
2023-10-15 14:10:18 +0500

    JHAB

--- src/app/components/profile.js
+++ src/app/components/profile.js
@@ -1,8 +1,8 @@
 'use client'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useMyContext } from '../context/context';
-import { faChartArea } from '@fortawesome/free-solid-svg-icons';
-import { faChartBar } from '@fortawesome/free-regular-svg-icons';
+import { faChartArea, faChartLine, faCheck, faClose, faCross, faLineChart } from '@fortawesome/free-solid-svg-icons';
+import { faBarChart, faChartBar } from '@fortawesome/free-regular-svg-icons';
 
 function User() {
     const { user } = useMyContext();
@@ -53,62 +53,58 @@ function User() {
                         <div class="px-6 py-6 bg-gray-100 border border-gray-300 rounded-lg shadow-xl">
                             <div class="flex items-center justify-between">
                                 <span class="font-bold text-sm text-indigo-600">Total Signals</span>
-                                <span class="text-xs bg-gray-200 hover:bg-gray-500 text-gray-500 hover:text-gray-200 px-2 py-1 rounded-lg transition duration-200 cursor-default">7 days</span>
+                                <span class="text-xs bg-gray-200 hover:bg-gray-500 text-gray-500 hover:text-gray-200 px-2 py-1 rounded-lg transition duration-200 cursor-default">Lifetime</span>
                             </div>
                             <div class="flex items-center justify-between mt-6">
                                 <div>
-                                <svg class="w-12 h-12 p-2.5 bg-indigo-400 bg-opacity-20 rounded-full text-indigo-600 border border-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-<FontAwesomeIcon icon={faChartBar}/>
-    <path d="M12 4a2 2 0 100 4 2 2 0 000-4zM19.59 12l-6.3-6.3a2 2 0 00-2.83 0l-6.3 6.3a2 2 0 000 2.83l6.3 6.3a2 2 0 002.83 0l6.3-6.3a2 2 0 000-2.83z" />
-</svg>
+                                    <svg class="w-12 h-12 p-2.5 bg-indigo-400 bg-opacity-20 rounded-full text-indigo-600 border border-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                        <FontAwesomeIcon icon={faLineChart} />
+                                        {/* <path d="M12 4a2 2 0 100 4 2 2 0 000-4zM19.59 12l-6.3-6.3a2 2 0 00-2.83 0l-6.3 6.3a2 2 0 000 2.83l6.3 6.3a2 2 0 002.83 0l6.3-6.3a2 2 0 000-2.83z" /> */}
+                                    </svg>
                                 </div>
                                 <div class="flex flex-col">
                                     <div class="flex items-end">
-                                        <span class="text-2xl 2xl:text-3xl font-bold">$8,141</span>
-                                        <div class="flex items-center ml-2 mb-1">
-                                            <svg class="w-5 h-5 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path></svg>
-                                            <span class="font-bold text-sm text-gray-500 ml-0.5">3%</span>
-                                        </div>
+                                        <span class="text-2xl 2xl:text-3xl font-bold">43.68k</span>
+
+
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div class="px-6 py-6 bg-gray-100 border border-gray-300 rounded-lg shadow-xl">
                             <div class="flex items-center justify-between">
-                                <span class="font-bold text-sm text-green-600">New Orders</span>
-                                <span class="text-xs bg-gray-200 hover:bg-gray-500 text-gray-500 hover:text-gray-200 px-2 py-1 rounded-lg transition duration-200 cursor-default">7 days</span>
+                                <span class="font-bold text-sm text-green-600">Good Signals</span>
+                                <span class="text-xs bg-gray-200 hover:bg-gray-500 text-gray-500 hover:text-gray-200 px-2 py-1 rounded-lg transition duration-200 cursor-default">Lifetime</span>
                             </div>
                             <div class="flex items-center justify-between mt-6">
                                 <div>
-                                    <svg class="w-12 h-12 p-2.5 bg-green-400 bg-opacity-20 rounded-full text-green-600 border border-green-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
+                                    <svg class="w-12 h-12 p-2.5 bg-green-400 bg-opacity-20 rounded-full text-green-600 border border-green-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <FontAwesomeIcon icon={faCheck} />
+                                    </svg>
                                 </div>
                                 <div class="flex flex-col">
                                     <div class="flex items-end">
-                                        <span class="text-2xl 2xl:text-3xl font-bold">217</span>
-                                        <div class="flex items-center ml-2 mb-1">
-                                            <svg class="w-5 h-5 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path></svg>
-                                            <span class="font-bold text-sm text-gray-500 ml-0.5">5%</span>
-                                        </div>
+                                        <span class="text-2xl 2xl:text-3xl font-bold">35.56k</span>
+
+
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div class="px-6 py-6 bg-gray-100 border border-gray-300 rounded-lg shadow-xl">
                             <div class="flex items-center justify-between">
-                                <span class="font-bold text-sm text-blue-600">New Connections</span>
-                                <span class="text-xs bg-gray-200 hover:bg-gray-500 text-gray-500 hover:text-gray-200 px-2 py-1 rounded-lg transition duration-200 cursor-default">7 days</span>
+                                <span class="font-bold text-sm text-red-600">Bad Signals</span>
+                                <span class="text-xs bg-gray-200 hover:bg-gray-500 text-gray-500 hover:text-gray-200 px-2 py-1 rounded-lg transition duration-200 cursor-default">Lifetime</span>
                             </div>
                             <div class="flex items-center justify-between mt-6">
                                 <div>
-                                    <svg class="w-12 h-12 p-2.5 bg-blue-400 bg-opacity-20 rounded-full text-blue-600 border border-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
+                                    <svg class="w-12 h-12 p-2.5 bg-red-400 bg-opacity-20 rounded-full text-red-600 border border-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <FontAwesomeIcon icon={faClose} />
+                                    </svg>
                                 </div>
                                 <div class="flex flex-col">
                                     <div class="flex items-end">
                                         <span class="text-2xl 2xl:text-3xl font-bold">54</span>
-                                        <div class="flex items-center ml-2 mb-1">
-                                            <svg class="w-5 h-5 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path></svg>
-                                            <span class="font-bold text-sm text-gray-500 ml-0.5">7%</span>
-                                        </div>
                                     </div>
                                 </div>
                             </div>
