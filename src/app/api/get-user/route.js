8d27a92739a27e4fea956cab0b6bda401251c99e
Abdullah <ssmmhazz@gmail.com>
2023-11-02 23:33:21 +0500

    'check'

--- src/app/api/get-user/route.js
+++ src/app/api/get-user/route.js
@@ -1,45 +1,34 @@
-import { NextResponse, NextRequest } from 'next/server';
-import Schemas from '../Modals/schemas.js';
+import { NextResponse, NextRequest } from 'next/server'
+import Schemas from '../Modals/schemas.js'
 import connectDB from '../db.js';
 
-connectDB();
+
+
+connectDB()
+
+
+
 
 export async function POST(req, res) {
-  try {
-    // Parse the request body
-    const uid = await req.json();
-    console.log(uid.uid);
+    try {
+        // Parse the request body
+        const uid = await req.json()
+        console.log(uid.uid);
 
-if (!uid) {
-  return NextResponse.json({ error: 'Invalid request data' });
-}
+        // Find the user by uid
+        const user = await Schemas.User.findOne({ fireBaseUid: uid.uid }).maxTime(30002);
 
-// Continue with data parsing and processing.
+        if (user) {
+            // Return the user data as JSON
+            return NextResponse.json(user);
+        } else {
+            // User not found, return an appropriate response
+            return NextResponse.json({ error: 'User not found' });
+        }
+    } catch (error) {
+        console.error('Error finding user:', error);
 
-
-    // Find the user by uid and populate the associated arrays
-    const user = await Schemas.User.findOne({ fireBaseUid: uid.uid })
-      .populate('Subscribers')
-      .populate('Subscribed')
-      .populate('expiredSignals')
-      .populate('activeSignals')
-      .populate('signalsPosted')
-      .populate('goodSignals')
-      .populate('badSignals')
-      .populate('neutralSignals')
-      .maxTime(30002);
-
-    if (user) {
-      // Return the user data with populated arrays as JSON
-      return NextResponse.json(user);
-    } else {
-      // User not found, return an appropriate response
-      return NextResponse.json({ error: 'User not found' });
+        // Send a JSON response with a 500 status code (Internal Server Error)
+        return NextResponse.json({ error: 'Internal Server Error' });
     }
-  } catch (error) {
-    console.error('Error finding user:', error);
-
-    // Send a JSON response with a 500 status code (Internal Server Error)
-    return NextResponse.json({ error: 'Internal Server Error' });
-  }
 }
