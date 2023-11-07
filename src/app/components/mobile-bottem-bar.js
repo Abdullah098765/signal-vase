daa1096a2cc7196a42c83fc83c903a1aa6d75eed
Abdullah <ssmmhazz@gmail.com>
2023-11-08 00:37:54 +0500

    bb

--- src/app/components/mobile-bottem-bar.js
+++ src/app/components/mobile-bottem-bar.js
@@ -27,28 +27,55 @@ function BottomNavbar() {
             window.removeEventListener('scroll', handleScroll);
         };
     }, [prevScrollPos]);
+
+    const [activeIcon, setActiveIcon] = useState('home'); // Set an initial active icon
+
+    const handleIconClick = (iconName) => {
+        setActiveIcon(iconName);
+    };
     return (
-        <nav className={`bottomBar bg-gray-900 text-white p-4 flex items-center justify-around fixed bottom-0 left-0 right-0 md:hidden ${visible ? 'bottom-navbar-visible' : 'bottom-navbar-hidden'}`}>
+        <nav className={`bottomBar bg-gray-900 text-white p-2 flex items-center justify-around fixed bottom-0 left-0 right-0 md:hidden ${visible ? 'bottom-navbar-visible' : 'bottom-navbar-hidden'}`}>
             {/* Home Icon */}
-            <button className="text-gray-400 hover:text-gray-100">
-
+            <button
+                className={`text-gray-400  ${activeIcon === 'home' ? 'bottam_icon' : ''}`}
+                onClick={() => handleIconClick('home')}
+            >
                 <FontAwesomeIcon icon={faHome} />
-
             </button>
 
             {/* Explore Icon */}
-            <button className="text-gray-400 hover:text-gray-100">
+            <button
+                className={`text-gray-400  ${activeIcon === 'following' ? 'bottam_icon' : ''}`}
+                onClick={() => handleIconClick('following')}
+            >
                 <FontAwesomeIcon icon={faHistory} />
             </button>
 
             {/* Notifications Icon */}
-            <button className="text-gray-400 hover:text-gray-100">
+            <button
+                className={`text-gray-400  ${activeIcon === 'notifications' ? 'bottam_icon' : ''}`}
+                onClick={() => handleIconClick('notifications')}
+            >
                 <FontAwesomeIcon icon={faBell} />
             </button>
 
             {/* Profile Icon */}
-            <button className="text-gray-400 hover:text-gray-100">
+            <button
+                className={`text-gray-400  ${activeIcon === 'profile' ? 'bottam_icon' : ''}`}
+                onClick={() => handleIconClick('profile')}
+            >
                 <FontAwesomeIcon icon={faUser} />
+            </button>
+            <button
+                className={`text-gray-400 signal-Icon ${activeIcon === 'signalHub' ? 'bottam_icon' : ''}`}
+                onClick={() => handleIconClick('signalHub')}
+            >
+
+                <svg className='text-gray-400' width={` ${activeIcon === 'signalHub' ? '20' : '16'}`} height={` ${activeIcon === 'signalHub' ? '20' : '16'}`} viewBox="0 0 67 79" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0.5 65.5L24.5 6.00001C27.5 2.00001 35 -4 42 6.00001L44.2389 11.5L65 62.5C67.5 73 65 75.5 58 78.5H11C4.5 77 0.5 73.5 0.5 65.5Z" fill="white" stroke="black" />
+                    <path d="M47.5 32C42.5 42 25.5 52 11.5 52L28.5 8.50001C30.1667 8.50001 32.5 4 37.5 8.50001L47.5 32Z" fill="#111827" stroke="black" />
+                </svg>
+
             </button>
         </nav>
     );
