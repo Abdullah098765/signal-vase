c84863f5c554384cdaf7c9e0360aa97262286888
Abdullah <ssmmhazz@gmail.com>
2023-11-10 14:40:41 +0500

    w

--- src/app/components/following-signal-CardList.js
+++ src/app/components/following-signal-CardList.js
@@ -123,7 +123,7 @@ const SignalCardList = () => {
                     </div>
                 </div>
             ))}
-            {(dataLoading && !user)  && <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-75 z-50">
+            {(dataLoading && user)  && <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-75 z-50">
                 <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
                 <p className="text-white ml-2 mt-4" >Loading...</p>
             </div>}
