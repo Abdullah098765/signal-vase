d2477f9603d272bda0b9bdb466805985521eeff3
Abdullah <ssmmhazz@gmail.com>
2023-11-11 12:43:14 +0500

    sm

--- src/app/signal/[id]/page.js
+++ src/app/signal/[id]/page.js
@@ -3,12 +3,16 @@ import Signal from './Signal'
 import { MyContextProvider } from '@/app/context/context'
 import Navbar from "../../components/navbar"
 import Modal from '@/app/components/signUp-Model'
+import Sidebar from '@/app/components/sidebar'
 export default function page() {
     return (
         <div>
             <MyContextProvider>
                 <Navbar />
+                <div className='flex'>
+                <Sidebar />
                 <Signal />
+                </div>
                 <Modal />
             </MyContextProvider>
         </div>
