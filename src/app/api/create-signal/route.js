ac36f0fd899eb6a6f199370126b1cebd2b237772
Abdullah <ssmmhazz@gmail.com>
2023-11-16 00:02:09 +0500

    POK

--- src/app/api/create-signal/route.js
+++ src/app/api/create-signal/route.js
@@ -23,6 +23,8 @@ export async function POST(req, res) {
     // Save the signal to the database
     var _signal = await newsignal.save();
     console.log('signal saved to the database', _signal);
+    await notifySubscribersAboutNewSignal(signalData);
+
     // Send a JSON response with a 200 status code (OK)
     return NextResponse.json(_signal);
   } catch (error) {
@@ -32,4 +34,27 @@ export async function POST(req, res) {
     return NextResponse.json({ error: 'An error occurred while saving the signal' }, { status: 500 });
   }
 }
+
+const notifySubscribersAboutNewSignal = async (signalData) => {
+  try {
+    // Make an HTTP request to your notification API
+    const notificationApiUrl = 'https://signal-hub.vercel.app/api/new-signal-notification';
+    const payload = {
+      signalId: signalData.signalId,
+      signalProviderId: signalData.signalProviderId,
+    };
+
+    await fetch(notificationApiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    });
+  } catch (error) {
+    console.error('Error notifying subscribers:', error);
+    // Handle error appropriately
+    throw error;
+  }
+};
 
--- src/app/api/new-signal-notification/route.js
+++ src/app/api/new-signal-notification/route.js
@@ -26,43 +26,59 @@ export async function POST(req, res) {
             return NextResponse.json({ error: 'Signal not found' });
         }
 
-        // Retrieve subscribers' FCM tokens from the signal provider's followers
-        const signalProvider = await models.User.findById(signalProviderId)
-        const subscribersFCMTokens = signalProvider.SubscribersFCMTokens
-        console.log(subscribersFCMTokens);
+        const signalProvider = await models.User.findById(signal.signalProvider._id).populate('Subscribers', 'notificationPreferences')
+        if (signalProvider.Subscribers && signalProvider.Subscribers.length > 0) {
+
+            const subscribersFCMTokens = signalProvider.Subscribers
+                .map(subscriber => subscriber.notificationPreferences?.fcmToken)
+                .filter(fcmToken => typeof fcmToken === 'string' && fcmToken.trim() !== '');
+
+            console.log(subscribersFCMTokens);
+
+            //later
+            // const subscribersFCMTokens = []
+            // signalProvider.Subscribers
+            //     .map(subscriber => {
+            //         subscribersFCMTokens.push(subscriber.notificationPreferences.fcmToken)
+            //     })
+            // console.log(signalProvider.Subscribers);
 
 
-        // Send notifications to subscribers
-        const notificationPayload = {
-            title: 'New Signal Posted!',
-            body: `Signal: ${signal.cryptoOrStock} ${signal.pair}`,
-        };
+            // Send notifications to subscribers
+            const notificationPayload = {
+                title: signalProvider.displayName,
+                body: `Posted new signal for : ${signal.cryptoOrStock} ${signal.pair}`,
+            };
 
-        if (subscribersFCMTokens.length > 0) {
-            const responses = await admin.messaging().sendMulticast({
-                tokens: subscribersFCMTokens,
-                notification: {
-                    title: notificationPayload.title,
-                    body: notificationPayload.body,
-                    // Add the URL you want to open on notification click
-                },
-                data: {
-                    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signal-hub-eb98f.appspot.com/o/profile-pictures%2F6541e74d50b66088ed303492%2FPhotoRoom-20230928_024430.png?alt=media&token=92554d0a-79cb-488f-a426-2ec9f7d27904',  // Add the URL of your image
-                    iconUrl: 'https://signal-hub.vercel.app/_next/static/media/logo.20d2465c.svg',       // Add the URL of your icon
-                    clickAction: '/signal/' + signalId,
-                },
-            });
-            responses.responses.forEach((response, index) => {
-                if (!response.success) {
-                    console.error(`Error sending notification to token ${subscribersFCMTokens[index]}:`, response.error);
-                }
-            });
-            console.log('Successfully sent notifications:', responses);
-            return NextResponse.json({ success: true, responses });
-        } else {
-            return NextResponse.json({ success: false, message: 'No subscribers to notify' });
+
+
+            if (subscribersFCMTokens.length > 0) {
+                const responses = await admin.messaging().sendMulticast({
+                    tokens: subscribersFCMTokens,
+                    notification: {
+                        title: notificationPayload.title,
+                        body: notificationPayload.body,
+                        // Add the URL you want to open on notification click
+                    },
+                    data: {
+                        imageUrl: "https://scontent.fkhi16-2.fna.fbcdn.net/v/t39.30808-6/402249571_888252269374154_4972497221506926378_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Emds1Tv4v38AX_Ojxcz&_nc_ht=scontent.fkhi16-2.fna&oh=00_AfAMweISCW8mhCm-PQTYuiAkMQl5h0o-DUps9djPKcP5BA&oe=65593852",
+                        iconUrl: signalProvider.profilePicture,
+                        // iconUrl: 'https://scontent.fkhi16-2.fna.fbcdn.net/v/t39.30808-6/402249571_888252269374154_4972497221506926378_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Emds1Tv4v38AX_Ojxcz&_nc_ht=scontent.fkhi16-2.fna&oh=00_AfAMweISCW8mhCm-PQTYuiAkMQl5h0o-DUps9djPKcP5BA&oe=65593852',       // Add the URL of your icon
+                        clickAction: '/signal/' + signalId,
+                    },
+                });
+                responses.responses.forEach((response, index) => {
+                    if (!response.success) {
+                        console.error(`Error sending notification to token ${subscribersFCMTokens[index]}:`, response.error);
+                    }
+                });
+                // console.log('Successfully sent notifications:', responses);
+                return NextResponse.json({ success: true, responses });
+            }
+            else {
+                return NextResponse.json({ success: false, message: 'No subscribers to notify' });
+            }
         }
-
     } catch (error) {
         console.error('Error:', error);
         return NextResponse.json({ error: 'Internal Server Error' });
