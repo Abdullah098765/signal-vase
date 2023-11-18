7d4cfbd8a5a42cac4ad8a85cdca755f91f99758d
Abdullah <ssmmhazz@gmail.com>
2023-11-19 00:58:09 +0500

    share

--- src/app/components/shareModal.js
+++ src/app/components/shareModal.js
@@ -14,10 +14,10 @@ const copyToClipboard = () => {
 
 
 return (
-    <div className={`fixed inset-0 z-50 ${isOpen ? 'block' : 'hidden'}`}>
-        <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
+    <div className={`fixed m-0 inset-0 z-50 ${isOpen ? 'block' : 'hidden'}`}>
+        <div className="absolute inset-0 bg-gray-900 m-0 opacity-50"></div>
 
-        <div className="bg-white w-96 relative z-10  mx-auto mt-20 p-6 rounded-lg shadow-lg">
+        <div className="bg-white w-96 relative z-10  mx-auto  mt-20 p-6 rounded-lg shadow-lg">
             <h2 className="text-2xl font-bold mb-4">Share this Signal</h2>
 
             {/* Share Buttons */}
