4e22cd5f70d8c1d9b5702644a5b2ec915b1f3282
Abdullah <ssmmhazz@gmail.com>
2023-11-19 23:03:01 +0500

    share

--- src/app/components/profile.js
+++ src/app/components/profile.js
@@ -162,7 +162,7 @@ function User() {
 
                             />
                             <div
-                                className=" text-gray-600 cursor-pointer rounded-md hover:text-gray-800 focus:outline-none"
+                                className=" text-gray-600 cursor-pointer  hidden md:block rounded-md hover:text-gray-800 focus:outline-none"
                                 onClick={openModal}
                             >
                                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
@@ -172,10 +172,18 @@ function User() {
                             <MyContextProvider>
                                 <EditButtons />
                                 {user && <EditProfileModal />}
+                                
 
                             </MyContextProvider>
 
-
+                            <div
+                                className=" text-gray-600 cursor-pointer mt-2  block md:hidden rounded-md hover:text-gray-800 focus:outline-none"
+                                onClick={openModal}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
+                                    <path d="M 18 2 A 3 3 0 0 0 15 5 A 3 3 0 0 0 15.054688 5.5605469 L 7.9394531 9.7109375 A 3 3 0 0 0 6 9 A 3 3 0 0 0 3 12 A 3 3 0 0 0 6 15 A 3 3 0 0 0 7.9355469 14.287109 L 15.054688 18.439453 A 3 3 0 0 0 15 19 A 3 3 0 0 0 18 22 A 3 3 0 0 0 21 19 A 3 3 0 0 0 18 16 A 3 3 0 0 0 16.0625 16.712891 L 8.9453125 12.560547 A 3 3 0 0 0 9 12 A 3 3 0 0 0 8.9453125 11.439453 L 16.060547 7.2890625 A 3 3 0 0 0 18 8 A 3 3 0 0 0 21 5 A 3 3 0 0 0 18 2 z"></path>
+                                </svg>
+                            </div>
 
                         </div>
                     </div>
