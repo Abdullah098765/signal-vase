1f6b5f08005f6018e3577d299f1b0005e995fda2
Abdullah <ssmmhazz@gmail.com>
2023-11-24 15:30:18 +0500

    subsc

--- src/app/components/cardList.js
+++ src/app/components/cardList.js
@@ -51,7 +51,7 @@ const SignalCardList = ({ }) => {
 
   return (
     <div className='webkit-fill-available'>
-      {signals[0] || true ? 
+      {signals[0]  ? 
         <div className="flex-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 p-0 md:p-2">
           {signals.map((signal) => (
             <Card signal={signal} key={signal._id} />
