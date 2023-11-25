a5a6c709255074c6b67e89bc32e892f942fa27cd
Abdullah <ssmmhazz@gmail.com>
2023-11-25 13:50:05 +0500

    notifications

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -48,15 +48,17 @@ self.addEventListener('notificationclick', (event) => {
   // Handle different action buttons
   if (action === 'goodSignal') {
     // Code to run when the "Good Signal" button is clicked
-    console.log('User clicked Good Signal' , clickAction.split('/')[2], clickAction.split('/')[3]);
+    console.log('User clicked Good Signal', clickAction.split('/')[2], clickAction.split('/')[3]);
     // Add your custom logic here
+    addGood(clickAction.split('/')[2], clickAction.split('/')[3])
   } else if (action === 'neutralSignal') {
     // Code to run when the "Neutral Signal" button is clicked
-    console.log('User clicked Neutral Signal',  clickAction.split('/')[2], clickAction.split('/')[3]);
+    console.log('User clicked Neutral Signal', clickAction.split('/')[2], clickAction.split('/')[3]);
+    addNeutral(clickAction.split('/')[2], clickAction.split('/')[3])
     // Add your custom logic here
   } else {
     // Code to run when the notification is clicked (not on an action button)
-    console.log('User clicked the notification',  clickAction.split('/')[2], clickAction.split('/')[3]);
+    console.log('User clicked the notification', clickAction.split('/')[2], clickAction.split('/')[3]);
     if (clickAction) {
       event.waitUntil(
         clients.openWindow(clickAction)
@@ -70,3 +72,53 @@ self.addEventListener('notificationclick', (event) => {
 
 });
 
+
+
+
+function addNeutral(signalId, userId,) {
+  var myHeaders = new Headers();
+  myHeaders.append("a", "dni");
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({
+    "signalId": signalId,
+    'neutralId': userId
+  });
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+  fetch("https://signal-hub.vercel.app/api/neutral-count", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+  return "Neutral counted"
+}
+function addGood(signalId, userId,) {
+  var myHeaders = new Headers();
+  myHeaders.append("a", "dni");
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({
+    "signalId": signalId,
+    'goodcounterId': userId
+  });
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+  fetch("https://signal-hub.vercel.app/api/good-count", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+  return "Good counted"
+
+}
