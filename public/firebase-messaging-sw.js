d228200c66af34d98bdd5b74bf877a82aea02bf2
Abdullah <ssmmhazz@gmail.com>
2023-11-25 12:57:15 +0500

    notifications

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
-import { addNeutral, addGood } from "../src/app/components/good-bad-buttons";
+importScripts('../src/app/components/good-bad-buttons'); // Import other scripts if needed
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('../firebase-messaging-sw.js')
     .then(function (registration) {
