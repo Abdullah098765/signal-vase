ff962a322ff6c2a8ebb83cd7da502e141a57bbda
Abdullah <ssmmhazz@gmail.com>
2023-12-03 13:30:00 +0500

    not

--- src/app/api/initialize/route.js
+++ src/app/api/initialize/route.js
@@ -3,12 +3,17 @@ import { setupChangeStream, cornInterval } from '../functions/functions'
 import connectDB from '../db';
 
 export async function POST(req, res) {
-connectDB()
+    try {
+        connectDB();
 
-    // Your initialization code here
-    setupChangeStream()
-    cornInterval()
-    console.log('Initialization code executed');
+        // Your initialization code here
+        await setupChangeStream();
+        await cornInterval();
+        console.log('Initialization code executed');
 
-    return NextResponse.json({ message: "Initialization code executed" });
+        return   await NextResponse.json({ message: "Initialization code executed" });
+    } catch (error) {
+        console.error('Initialization error:', error);
+      return  await NextResponse.json({ error: "Initialization error" });
+    }
 }
