92491ae7c0acd7d9b04ff5d00f94825d06c1c270
Abdullah <ssmmhazz@gmail.com>
2023-12-09 00:32:26 +0500

    subs

--- src/app/subscription/Subscription.js
+++ src/app/subscription/Subscription.js
@@ -6,14 +6,10 @@ import { MyContextProvider, useMyContext } from '../context/context.js';
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-
 function Subscription() {
-
-    const [subscriptions, setSubscriptions] = useState()
-    const [dataLoading, setDataLoading] = useState(true)
+    const [subscriptions, setSubscriptions] = useState([]);
+    const [dataLoading, setDataLoading] = useState(true);
     const { user, setRouterLoading, isModalOpen, setIsModalOpen, selectedSignal, setSelectedSignal, isSignalModalOpen, setisSignalModalOpen, getSignals } = useMyContext();
-
-
 
     useEffect(() => {
         const fetchData = async () => {
@@ -31,7 +27,7 @@ function Subscription() {
                 if (response.ok) {
                     const data = await response.json();
                     setSubscriptions(data);
-                    setDataLoading(false)
+                    setDataLoading(false);
                     console.log(data);
                 } else {
                     console.error('Failed to fetch subscribed data');
@@ -46,36 +42,35 @@ function Subscription() {
             fetchData();
         }
     }, [user]);
+
     return (
         <MyContextProvider>
             <div className='webkit-fill-available h-full'>
-                <div className='webkit-fill-available h-full'>
-                    {!dataLoading ?
-                        <div className="flex-1 grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-1 p-0 md:p-2">
+                {!dataLoading ? (
+                    <div className="flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1 p-0 md:p-2">
+                        {subscriptions.map((subscription) => (
+                            <SubscribedCard subscribed={subscription} key={subscription._id} />
+                        ))}
+                    </div>
+                ) : (
+                    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-75 z-50">
+                        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
+                        <p className="text-white mt-4">Loading...</p>
+                    </div>
+                )}
 
-                            {subscriptions.map((subscription) => (
-                                <SubscribedCard subscribed={subscription} key={subscription._id} />
-                            ))}
-                        </div> : <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-75 z-50">
-                            <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
-                            <p className="text-white mt-4">Loading...</p>
+                {subscriptions.length === 0 && !dataLoading && (
+                    <div className='webkit-fill-available h-full flex justify-center items-center mt-44 text-gray-800'>
+                        <div className="flex flex-col items-center">
+                            <FontAwesomeIcon icon={faInfoCircle} className="text-4xl mb-2" />
+                            <p className="text-lg font-semibold">No Subscriptions</p>
+                            <p className="text-sm">Explore and subscribe providers to get signals.</p>
                         </div>
-
-                    }
-
-                    {subscriptions?.length === 0 && !dataLoading && (
-                        <div className='webkit-fill-available h-full flex justify-center items-center mt-44 text-gray-800'>
-                            <div className="flex flex-col items-center">
-                                <FontAwesomeIcon icon={faInfoCircle} className="text-4xl mb-2" />
-                                <p className="text-lg font-semibold">No Subscriptions</p>
-                                <p className="text-sm">Explore and subscribe providers to get signals.</p>
-                            </div>
-                        </div>
-                    )}
-                </div>
+                    </div>
+                )}
             </div>
         </MyContextProvider>
-    )
+    );
 }
 
-export default Subscription
+export default Subscription;
