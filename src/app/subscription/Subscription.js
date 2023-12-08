f2904bcc438c98b1479ec42aaf13afa8fc8ee5db
Abdullah <ssmmhazz@gmail.com>
2023-12-08 23:51:03 +0500

    subs

--- src/app/subscription/Subscription.js
+++ src/app/subscription/Subscription.js
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from 'react'
 import SubscriptionCard from "./SubscriptionsCard.js"
-import { useMyContext } from '../context/context.js';
+import { MyContextProvider, useMyContext } from '../context/context.js';
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -46,31 +46,34 @@ function Subscription() {
         }
     }, [user]);
     return (
-        <div className='webkit-fill-available h-full'>
+        <MyContextProvider>
             <div className='webkit-fill-available h-full'>
-                {subscriptions ?
-                    <div className="flex-1 grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-1 p-0 md:p-2">
+                <div className='webkit-fill-available h-full'>
+                    {subscriptions ?
+                        <div className="flex-1 grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-1 p-0 md:p-2">
 
-                        {subscriptions.map((subscription) => (
-                            <SubscriptionCard subscribed={subscription} key={subscription._id} />
-                        ))}
-                    </div> : <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-75 z-50">
-                        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
-                        <p className="text-white mt-4">Loading...</p>
-                    </div>
-
-                }
-
-                {subscriptions.length === 0 && (
-                    <div className='webkit-fill-available h-full flex justify-center items-center mt-44 text-gray-800'>
-                        <div className="flex flex-col items-center">
-                            <FontAwesomeIcon icon={faInfoCircle} className="text-4xl mb-2" />
-                            <p className="text-lg font-semibold">No Subscriptions</p>
-                            <p className="text-sm">Explore and subscribe providers to get signals.</p>
+                            {subscriptions.map((subscription) => (
+                                <SubscriptionCard subscribed={subscription} key={subscription._id} />
+                            ))}
+                        </div> : <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-75 z-50">
+                            <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
+                            <p className="text-white mt-4">Loading...</p>
                         </div>
-                    </div>
-                )}
-            </div>        </div>
+
+                    }
+
+                    {subscriptions.length === 0 && (
+                        <div className='webkit-fill-available h-full flex justify-center items-center mt-44 text-gray-800'>
+                            <div className="flex flex-col items-center">
+                                <FontAwesomeIcon icon={faInfoCircle} className="text-4xl mb-2" />
+                                <p className="text-lg font-semibold">No Subscriptions</p>
+                                <p className="text-sm">Explore and subscribe providers to get signals.</p>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </MyContextProvider>
     )
 }
 
