91ed2a45657b2532b4dab35f83339f0ede9721dd
Abdullah <ssmmhazz@gmail.com>
2023-12-09 00:51:45 +0500

    subs

--- src/app/subscription/Subscription.js
+++ src/app/subscription/Subscription.js
@@ -9,6 +9,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function Subscription() {
     const [subscriptions, setSubscriptions] = useState([]);
     const [dataLoading, setDataLoading] = useState(true);
+    const [loggedIn, setLoggedIn] = useState(true);
     const { user, setRouterLoading, isModalOpen, setIsModalOpen, selectedSignal, setSelectedSignal, isSignalModalOpen, setisSignalModalOpen, getSignals } = useMyContext();
 
     useEffect(() => {
@@ -48,30 +49,37 @@ function Subscription() {
 
     return (
         <MyContextProvider>
-            <div className='webkit-fill-available h-full'>
-                {!dataLoading ? (
-                    <div className="flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1 p-0 md:p-2">
-                        {subscriptions.map((subscription) => (
-                            <SubscribedCard subscribed={subscription} key={subscription._id} />
-                        ))}
-                    </div>
-                ) : (
-                    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-75 z-50">
-                        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
-                        <p className="text-white mt-4">Loading...</p>
-                    </div>
-                )}
-
-                {subscriptions.length === 0 && !dataLoading && (
-                    <div className='webkit-fill-available h-full flex justify-center items-center mt-44 text-gray-800'>
-                        <div className="flex flex-col items-center">
-                            <FontAwesomeIcon icon={faInfoCircle} className="text-4xl mb-2" />
-                            <p className="text-lg font-semibold">No Subscriptions</p>
-                            <p className="text-sm">Explore and subscribe providers to get signals.</p>
+            {loggedIn ?
+                <div className='webkit-fill-available h-full'>
+                    {!dataLoading ? (
+                        <div className="flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1 p-0 md:p-2">
+                            {subscriptions.map((subscription) => (
+                                <SubscribedCard subscribed={subscription} key={subscription._id} />
+                            ))}
                         </div>
+                    ) : (
+                        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-75 z-50">
+                            <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
+                            <p className="text-white mt-4">Loading...</p>
+                        </div>
+                    )}
+
+                    {subscriptions.length === 0 && !dataLoading && (
+                        <div className='webkit-fill-available h-full flex justify-center items-center mt-44 text-gray-800'>
+                            <div className="flex flex-col items-center">
+                                <FontAwesomeIcon icon={faInfoCircle} className="text-4xl mb-2" />
+                                <p className="text-lg font-semibold">No Subscriptions</p>
+                                <p className="text-sm">Explore and subscribe providers to get signals.</p>
+                            </div>
+                        </div>
+                    )}
+                </div> :
+                <> <div className='webkit-fill-available h-full flex justify-center items-center mt-44 text-gray-800'>
+                    <div onClick={()=> setIsModalOpen(true)} className="flex flex-col items-center">
+                        Sign In
                     </div>
-                )}
-            </div>
+                </div></>
+            }
         </MyContextProvider>
     );
 }
--- src/app/subscription/page.js
+++ src/app/subscription/page.js
@@ -4,17 +4,21 @@ import { MyContextProvider } from '../context/context'
 import Navbar from '../components/navbar'
 import Sidebar from '../components/sidebar'
 import RouterLoading from '../components/routerLoading'
+import Modal from '../components/signUp-Model'
+import BottomNavbar from '../components/mobile-bottem-bar'
 
 function page() {
     return (
         <div>
-             <MyContextProvider>
-                <RouterLoading/>
-                <Navbar/>
+            <MyContextProvider>
+                <RouterLoading />
+                <Navbar />
                 <div className='flex '>
-                <Sidebar />
-                <Subscription/>
+                    <Sidebar />
+                    <Subscription />
                 </div>
+                <Modal />
+                <BottomNavbar />
             </MyContextProvider>
         </div>
     )
