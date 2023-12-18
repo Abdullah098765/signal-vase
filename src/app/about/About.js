e2db26fc3f408415fec4776cb4aa1bc70f411f53
Abdullah Sabir <ssmmhazz@gmail.com>
2023-12-18 08:44:10 -0700

    aboua

--- src/app/about/About.js
+++ src/app/about/About.js
@@ -16,28 +16,28 @@ const About = () => {
                               <ul className="list-disc list-inside">
                                     <li className="mb-2">Diverse Signal Providers</li>
                                     <p className="text-lg text-gray-700 mb-4">
-                                          Signal Hub brings together a diverse community of signal providers, each with their unique trading strategies and expertise. We understand that every trader is different, and that's why we provide you with the flexibility to choose from a variety of signals tailored to your preferences and risk tolerance.
+                                          {`    Signal Hub brings together a diverse community of signal providers, each with their unique trading strategies and expertise. We understand that every trader is different, and that's why we provide you with the flexibility to choose from a variety of signals tailored to your preferences and risk tolerance.`}
                                     </p>
                                     <li className="mb-2">Transparent Reviews</li>
                                     <p className="text-lg text-gray-700 mb-4">
-                                          We understand the importance of trust in the trading community. Signal Hub allows users to provide genuine reviews of signal providers, creating a transparent environment where traders can make informed decisions based on real experiences.                                    </p>
+                                          {`       We understand the importance of trust in the trading community. Signal Hub allows users to provide genuine reviews of signal providers, creating a transparent environment where traders can make informed decisions based on real experiences.         `}                           </p>
                                     <li className="mb-2">Scam Protection</li>
                                     <p className="text-lg text-gray-700 mb-4">
-                                          Our primary goal is to shield users from scams prevalent on social media platforms. Signal Hub is a secure space designed to eliminate the risks associated with unreliable signals, providing you with a trustworthy platform to enhance your trading journey.                                    </p>
-                                    <li className="mb-2">EtherealCash Integration (Under Development!)</li>
+                                          {`        Our primary goal is to shield users from scams prevalent on social media platforms. Signal Hub is a secure space designed to eliminate the risks associated with unreliable signals, providing you with a trustworthy platform to enhance your trading journey.       `}                             </p>
+                                    <li className="mb-2">{`EtherealCash Integration (Under Development!)`}</li>
                                     <p className="text-lg text-gray-700 mb-4">
-                                          To facilitate seamless transactions between signal providers and followers, Signal Hub introduces EtherealCash, a digital currency exclusive to our platform. Users can buy and sell signals using EtherealCash, ensuring a convenient and secure payment system.                                    </p>
+                                          {`  To facilitate seamless transactions between signal providers and followers, Signal Hub introduces EtherealCash, a digital currency exclusive to our platform. Users can buy and sell signals using EtherealCash, ensuring a convenient and secure payment system.    `}                                </p>
 
                               </ul>
                         </div>
 
                         <p className="text-lg text-gray-700 mb-8">
-                              Whether you're a seasoned trader looking to share your expertise or a newcomer seeking reliable signals, Signal Hub welcomes you to a community driven by trust, transparency, and innovation. Together, let's redefine the trading landscape and ensure that every trader gets what they deserve.
+                              {`    Whether you're a seasoned trader looking to share your expertise or a newcomer seeking reliable signals, Signal Hub welcomes you to a community driven by trust, transparency, and innovation. Together, let's redefine the trading landscape and ensure that every trader gets what they deserve.
 
-                        </p>
+`}           </p>
 
                         <p className="text-lg text-gray-700">
-                              Signal Hub - Trading with Trust, Trading with Signal Hub.
+                              {`          Signal Hub - Trading with Trust, Trading with Signal Hub.`}
                         </p>
                   </div>
             </div>
