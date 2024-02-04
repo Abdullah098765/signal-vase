d7ee344df168dbb4498722c1789d95bcc8b233fb
Abdullah Sabir <ssmmhazz@gmail.com>
2024-02-04 01:59:53 -0700

    cornJobs

--- src/app/api/finding-expired-Signals/route.js
+++ src/app/api/finding-expired-Signals/route.js
@@ -34,106 +34,107 @@ const buttons = [
 connectDB();
 
 export async function POST(req, res) {
-  setInterval(async () => {
-    try {
-      // const { skip } = await req.json();
+  try {
+    // const { skip } = await req.json();
 
 
-      const currentTimeInMillis = new Date().getTime();
+    const currentTimeInMillis = new Date().getTime();
 
-      // const notificationDataFromExpiredSignals = await schemas.Signal.aggregate([
-      //   {
-      //     $match: {
-      //       duration: { $lt: currentTimeInMillis },
-      //       expirationNotificationSent: false
-      //     }
-      //   },
-      //   {
-      //     $unwind: '$followers'
-      //   },
-      //   {
-      //     $lookup: {
-      //       from: 'users', // Replace with the actual name of your followers collection
-      //       localField: 'followers',
-      //       foreignField: '_id',
-      //       as: 'followerData'
-      //     }
-      //   },
-      //   {
-      //     $unwind: '$followerData'
-      //   },
-      //   {
-      //     $project: {
-      //       _id: 1,
-      //       pair: 1,
-      //       cryptoOrStock: 1,
-      //       'follower._id': '$followerData._id',
-      //       'follower.notificationPreferences': '$followerData.notificationPreferences'
-      //     }
-      //   }
-      // ]);
+    // const notificationDataFromExpiredSignals = await schemas.Signal.aggregate([
+    //   {
+    //     $match: {
+    //       duration: { $lt: currentTimeInMillis },
+    //       expirationNotificationSent: false
+    //     }
+    //   },
+    //   {
+    //     $unwind: '$followers'
+    //   },
+    //   {
+    //     $lookup: {
+    //       from: 'users', // Replace with the actual name of your followers collection
+    //       localField: 'followers',
+    //       foreignField: '_id',
+    //       as: 'followerData'
+    //     }
+    //   },
+    //   {
+    //     $unwind: '$followerData'
+    //   },
+    //   {
+    //     $project: {
+    //       _id: 1,
+    //       pair: 1,
+    //       cryptoOrStock: 1,
+    //       'follower._id': '$followerData._id',
+    //       'follower.notificationPreferences': '$followerData.notificationPreferences'
+    //     }
+    //   }
+    // ]);
 
-      const signals = await schemas.Signal.find({
-        duration: { $lt: currentTimeInMillis },
-        expirationNotificationSent: false,
-        $expr: { $gt: [{ $size: "$followers" }, 0] }
-      }).populate({
-        path: 'followers',
-        select: 'notificationPreferences _id'
-      });
+    const signals = await schemas.Signal.find({
+      duration: { $lt: currentTimeInMillis },
+      expirationNotificationSent: false,
+      $expr: { $gt: [{ $size: "$followers" }, 0] }
+    }).populate({
+      path: 'followers',
+      select: 'notificationPreferences _id'
+    });
 
 
-      if (signals) {
+    if (signals) {
 
-        var followersIds = []
-        var followersFCMTokens = []
-        for (let i = 0; i < signals.length; i++) {
-          const signal = signals[i];
-          for (let j = 0; j < signal.followers.length; j++) {
-            const follower = signal.followers[j];
-            followersIds.push(follower._id)
-            if (follower.notificationPreferences.inApp) {
-              followersFCMTokens.push(follower.notificationPreferences.fcmToken)
-            }
-
+      var followersIds = []
+      var followersFCMTokens = []
+      for (let i = 0; i < signals.length; i++) {
+        const signal = signals[i];
+        for (let j = 0; j < signal.followers.length; j++) {
+          const follower = signal.followers[j];
+          followersIds.push(follower._id)
+          if (follower.notificationPreferences.inApp) {
+            followersFCMTokens.push(follower.notificationPreferences.fcmToken)
           }
-          const buttonsData = buttons.map(button => ({
-            title: button.title,
-            action: button.action,
-          }));
 
-          const message = {
-            tokens: followersFCMTokens,
-            notification: {
-              title: 'Signal Expiration',
-              body: `The signal for ${signal.cryptoOrStock} ${signal.pair} has expired.`,
-            },
-            data: {
-              clickAction: `/signal/${signal._id}/`, // Note: You might need to adjust this part if followerId is not accessible here
-              buttons: JSON.stringify(buttonsData),
-              receiverId: JSON.stringify(followersIds),
-            },
-          };
-
-          var savedNotification = await saveNotificationData(message)
-          var notificationSending = await pushNotification(message)
-          console.log(notificationSending, savedNotification);
         }
-        console.log('Interval Is On!');
+        console.log("followersIds: ", followersIds);
+        console.log("followersFCMTokens: ", followersFCMTokens);
+        const buttonsData = buttons.map(button => ({
+          title: button.title,
+          action: button.action,
+        }));
 
-      } else {
-        // signals not found, return an appropriate response
-        return new Response(JSON.stringify({ error: "signals not found" }));
+        const message = {
+          tokens: followersFCMTokens,
+          notification: {
+            title: 'Signal Expiration',
+            body: `The signal for ${signal.cryptoOrStock} ${signal.pair} has expired.`,
+          },
+          data: {
+            clickAction: `/signal/${signal._id}/`, // Note: You might need to adjust this part if followerId is not accessible here
+            buttons: JSON.stringify(buttonsData),
+            receiverId: JSON.stringify(followersIds),
+          },
+        };
+
+        var savedNotification = await saveNotificationData(message)
+        var notificationSending = await pushNotification(message)
+        return new Response(JSON.stringify({ notificationSending, savedNotification }));
+
       }
-    } catch (error) {
-      console.error("Error finding signalsr:", error);
 
-      // Send a JSON response with a 500 status code (Internal Server Error)
-      return new Response(JSON.stringify({ error: "Internal Server Error" }));
+      return new Response(JSON.stringify(signals));
+
+
+    } else {
+      // signals not found, return an appropriate response
+      return new Response(JSON.stringify({ error: "signals not found" }));
     }
-  }, 10000);
-  return new Response(JSON.stringify({ Status: "Interval In On!" }));
+  } catch (error) {
+    console.error("Error finding signalsr:", error);
 
+    // Send a JSON response with a 500 status code (Internal Server Error)
+    return new Response(JSON.stringify({ error: "Internal Server Error" }));
+  }
 }
 
 
@@ -151,7 +152,7 @@ const saveNotificationData = async (message) => {
     const newNotification = new Schemas.Notification(notificationData);
     const savedNotification = await newNotification.save();
 
- //   console.log('Notification saved to the database', savedNotification);
+    console.log('Notification saved to the database', savedNotification);
     // You can perform additional logic here if needed
     await Schemas.Signal.updateOne(
       { _id: message.data.clickAction.split('/')[2] }, // Assuming the signal is within the savedNotification object
@@ -173,7 +174,7 @@ const pushNotification = async (message) => {
     const response = await admin.messaging().sendMulticast(message);
 
     if (response.successCount > 0) {
-  //    console.log(`${response.successCount} messages were sent successfully.`);
+      console.log(`${response.successCount} messages were sent successfully.`);
 
       return `${response.successCount} messages were sent successfully.`
     }
