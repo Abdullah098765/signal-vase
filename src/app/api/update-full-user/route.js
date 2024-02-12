d6501d400b8ff7c05854c6ed7ca0e2b5d0b3a16a
Abdullah Sabir <ssmmhazz@gmail.com>
2024-02-12 02:40:09 -0700

    update

--- src/app/api/update-full-user/route.js
+++ src/app/api/update-full-user/route.js
@@ -7,16 +7,15 @@ connectDB();
 export async function PUT(req, res) {
     try {
         // Parse the request body
-        const user = await req.json();
+        const {user} = await req.json();
         // const { displayName, profilePicture, bio, about, uid, market } = userData;
-        console.log(user);
         // You may want to validate the incoming data here
 
         // Assuming you have a function like `updateUser` to update user data
         const updatedUser = await Schemas.User.findByIdAndUpdate(user._id, user, { new: true });
 
         if (updatedUser) {
-            return NextResponse.json(updatedUser);
+            return NextResponse.json({message:"user updated"});
         } else {
             return NextResponse.json({ error: 'User not found' });
         }
