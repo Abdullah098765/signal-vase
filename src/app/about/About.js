f15b6c52631ce31ad25193797788d332235a08c1
Abdullah Sabir <ssmmhazz@gmail.com>
2024-03-06 10:05:59 -0700

    as

--- src/app/about/About.js
+++ src/app/about/About.js
@@ -10,7 +10,7 @@ const About = () => {
     const actionCodeSettings = {
       // URL you want to redirect back to. The domain (www.example.com) for this
       // URL must be in the authorized domains list in the Firebase Console.
-      url: 'https://www.noarway.com',
+      url: 'https://signal-hub.vercel.app/profile',
       // This must be true.
       handleCodeInApp: true,
 
