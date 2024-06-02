e007dbf29b3c78906140d2253a7e597f3b641644
Abdullah Sabir <ssmmhazz@gmail.com>
2024-06-03 02:51:42 +0500

    Vase

--- src/app/about/About.js
+++ src/app/about/About.js
@@ -4,19 +4,19 @@ const About = () => {
   return (
     <div className="bg-gray-100 p-8 w-full">
       <div className="max-w-4xl mx-auto">
-        <h1 className="text-4xl font-bold mb-6">About Signal Hub</h1>
+        <h1 className="text-4xl font-bold mb-6">About Signal Vase</h1>
         <hr />
 
         <p className="text-lg text-gray-700 mb-8">
-          Welcome to Signal Hub - Your Gateway to Informed Trading!
-          <br /> At Signal Hub, we believe in empowering individuals to navigate
+          Welcome to Signal Vase - Your Gateway to Informed Trading!
+          <br /> At Signal Vase, we believe in empowering individuals to navigate
           the complex world of trading with confidence and security.
         </p>
 
         <div className="mb-8">
-          <h2 className="text-2xl font-bold mb-4">Support Signal Hub</h2>
+          <h2 className="text-2xl font-bold mb-4">Support Signal Vase</h2>
           <p className="text-lg text-gray-700 mb-4">
-            If you find Signal Hub valuable and would like to support us, you can contribute by sending ETH to our MetaMask wallet address:
+            If you find Signal Vase valuable and would like to support us, you can contribute by sending ETH to our MetaMask wallet address:
             <br />
             <strong>MetaMask Wallet Address:</strong> 0xa14E189aB649421466435Ed79C0a2B711b71e679
           </p>
@@ -26,36 +26,36 @@ const About = () => {
         <div className="mb-8">
           <h2 className="text-2xl font-bold mb-4">Support the Development of Ethereal Cash (ERCH) And Mobile App</h2>
           <p className="text-lg text-gray-700 mb-4">
-            We want to keep you informed about the development of ERCH and Signal Hub mobile application. Currently, progress is hindered due to limited funds. The immobility in {"ERCH's"} development is a direct result of financial constraints.
+            We want to keep you informed about the development of ERCH and Signal Vase mobile application. Currently, progress is hindered due to limited funds. The immobility in {"ERCH's"} development is a direct result of financial constraints.
           </p>
           <p className="text-lg text-gray-700 mb-4">
-            Your support can make a significant difference. By contributing to Signal Hub, you are not only supporting the platform itself but also helping to accelerate the development of ERCH and its mobile app. Together, we can overcome these challenges and continue to innovate in the world of trading.
+            Your support can make a significant difference. By contributing to Signal Vase, you are not only supporting the platform itself but also helping to accelerate the development of ERCH and its mobile app. Together, we can overcome these challenges and continue to innovate in the world of trading.
           </p>
         </div>
         <div className="mb-8">
-          <h2 className="text-2xl font-bold mb-4">Why Signal Hub?</h2>
+          <h2 className="text-2xl font-bold mb-4">Why Signal Vase?</h2>
           <ul className="list-disc list-inside">
             <li className="mb-2">Diverse Signal Providers</li>
             <p className="text-lg text-gray-700 mb-4">
-              {`    Signal Hub brings together a diverse community of signal providers, each with their unique trading strategies and expertise. We understand that every trader is different, and that's why we provide you with the flexibility to choose from a variety of signals tailored to your preferences and risk tolerance.`}
+              {`    Signal Vase brings together a diverse community of signal providers, each with their unique trading strategies and expertise. We understand that every trader is different, and that's why we provide you with the flexibility to choose from a variety of signals tailored to your preferences and risk tolerance.`}
             </p>
             <li className="mb-2">Transparent Reviews</li>
             <p className="text-lg text-gray-700 mb-4">
-              {`       We understand the importance of trust in the trading community. Signal Hub allows users to provide genuine reviews of signal providers, creating a transparent environment where traders can make informed decisions based on real experiences.         `}{" "}
+              {`       We understand the importance of trust in the trading community. Signal Vase allows users to provide genuine reviews of signal providers, creating a transparent environment where traders can make informed decisions based on real experiences.         `}{" "}
             </p>
             <li className="mb-2">Scam Protection</li>
             <p className="text-lg text-gray-700 mb-4">
-              {`        Our primary goal is to shield users from scams prevalent on social media platforms. Signal Hub is a secure space designed to eliminate the risks associated with unreliable signals, providing you with a trustworthy platform to enhance your trading journey.       `}{" "}
+              {`        Our primary goal is to shield users from scams prevalent on social media platforms. Signal Vase is a secure space designed to eliminate the risks associated with unreliable signals, providing you with a trustworthy platform to enhance your trading journey.       `}{" "}
             </p>
             <li className="mb-2">{`EtherealCash Integration (Under Development!)`}</li>
             <p className="text-lg text-gray-700 mb-4">
-              {`  To facilitate seamless transactions between signal providers and followers, Signal Hub introduces EtherealCash, a digital currency exclusive to our platform. Users can buy and sell signals using EtherealCash, ensuring a convenient and secure payment system.    `}{" "}
+              {`  To facilitate seamless transactions between signal providers and followers, Signal Vase introduces EtherealCash, a digital currency exclusive to our platform. Users can buy and sell signals using EtherealCash, ensuring a convenient and secure payment system.    `}{" "}
             </p>
           </ul>
         </div>
 
         <p className="text-lg text-gray-700 mb-8">
-          {`    Whether you're a seasoned trader looking to share your expertise or a newcomer seeking reliable signals, Signal Hub welcomes you to a community driven by trust, transparency, and innovation. Together, let's redefine the trading landscape and ensure that every trader gets what they deserve.`}{" "}
+          {`    Whether you're a seasoned trader looking to share your expertise or a newcomer seeking reliable signals, Signal Vase welcomes you to a community driven by trust, transparency, and innovation. Together, let's redefine the trading landscape and ensure that every trader gets what they deserve.`}{" "}
         </p>
         <div className="mb-8">
           <h2 className="text-2xl font-bold mb-4">Contact Us</h2>
@@ -66,7 +66,7 @@ const About = () => {
           {/* Add your contact details or a contact form here */}
         </div>
         <p className="text-lg text-gray-700">
-          {`          Signal Hub - Trading with Trust, Trading with Signal Hub.`}
+          {`          Signal Vase - Trading with Trust, Trading with Signal Vase.`}
         </p>
       </div>
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,23 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Signal Hubs',
-  description: 'Signal Hubs is a Trading platform',
- 
-  
-}
+  title: "Signal Vase",
+  description:
+    "Signal Vase is a comprehensive trading platform designed to empower users with accurate trading signals across various financial markets including currency, gold, crypto, and forex. With Signal Vase, users can access a diverse range of signals provided by experienced traders, enabling them to make informed trading decisions and potentially enhance their financial success. The platform also features a robust review system where users can rate signal providers based on their performance, helping to foster transparency and accountability within the trading community. Whether you're a seasoned trader looking for reliable signals or a novice seeking guidance, Signal Vase offers a user-friendly and secure environment to explore and engage in the world of trading.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
