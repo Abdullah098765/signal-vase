49aa6ccd5603910bf533cef690c452c3a48ed286
AbdullahSabir098765 <ssmmhazz@gmail.com>
2024-10-22 14:59:16 +0500

    landing page replacing

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,7 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://signalvase.com', // Replace with your domain
-    generateRobotsTxt: true, // (optional) Generates a `robots.txt` file
-    // Additional options can be set here, such as exclusions
-  };
-  
+  siteUrl: 'http://signalvase.com',
+  generateRobotsTxt: true, // Generates a `robots.txt` file
+};
